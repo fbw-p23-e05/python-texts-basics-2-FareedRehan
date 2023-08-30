c460ac53e3a918e6cfba4f6359db5a93b3095725
FareedRehan <fareed.rehan@dci-student.org>
2023-08-30 17:20:07 +0200

    exercise-2.py added

---
+++ exercise-2.py
@@ -0,0 +1,28 @@
+# TASK 1
+text = "Berlin is a world city of culture, politics, media and science."
+print(len(text))
+
+# TASK 2
+text = "Berlin is a world city of culture, politics, media and science."
+first_character = text[0]
+last_character = text[-1]
+print(first_character + " "+last_character)
+
+# TASK 3
+text = "Berlin is a world city of culture, politics, media and science."
+print(text[0:3].upper())
+
+# TASK 4
+text = "Berlin is a world city of culture, politics, media and science."
+print(text[0:3].upper())
+
+# TASK 5
+text = "Berlin is surrounded by the State of Brandenburg and contiguous with Potsdam, Brandenburg's capital"
+result = text.count("B")
+print("B appears:", result, "times")
+
+# TASK 6
+first_name = "Mary"
+last_name = "Mat"
+print("Firstname:", first_name)
+print("Lastname:", last_name)
